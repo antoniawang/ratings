8f6e86e7a630af09afcaa081f505b5d84bc066e6
Hackbright Student <info@hackbrightacademy.com>
2015-07-27 17:51:10 -0700

    revised seed.py slightly

--- seed.py
+++ seed.py
@@ -5,10 +5,10 @@ from server import app
 import time
 import datetime
 
-def load_users():
+def load_users(filename):
     """Load users from u.user into database."""
 
-    users_file = open('seed_data/u.user')
+    users_file = open(filename)
 
     for line in users_file:
         line = line.strip()
@@ -16,14 +16,13 @@ def load_users():
 
         new_user = User(age=user_record[2], zipcode=user_record[4])
         db.session.add(new_user)
-       
+    db.session.commit()
 
 
-
-def load_movies():
+def load_movies(filename):
     """Load movies from u.item into database."""
 
-    items_file = open('seed_data/u.item')
+    items_file = open(filename)
 
     for line in items_file:
         line = line.strip()
@@ -35,18 +34,16 @@ def load_movies():
             date_time = datetime.datetime.strptime(date.strip(), "%d-%b-%Y")
 
         
-
-        
         new_movie = Movie(movie_id=movie_record[0], title=movie_title_clean,
             released_at=date_time, imdb_url=movie_record[3])
         db.session.add(new_movie)
-    
+    db.session.commit()
 
         
-def load_ratings():
+def load_ratings(filename):
     """Load ratings from u.data into database."""
 
-    ratings_file = open('seed_data/u.data')
+    ratings_file = open(filename)
 
     for line in ratings_file:
         line = line.strip()
@@ -56,14 +53,12 @@ def load_ratings():
             score=rating_record[2])
 
         db.session.add(new_rating)
-        
-
+    db.session.commit()
 
 
 if __name__ == "__main__":
     connect_to_db(app)
 
-    load_users()
-    load_movies()
-    load_ratings() 
-    db.session.commit()
+    load_users('seed_data/u.user')
+    load_movies('seed_data/u.item')
+    load_ratings('seed_data/u.data') 
